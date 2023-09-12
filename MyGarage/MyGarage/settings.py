9a54d25d47d10e49452450cd8c921a5d86f7b62b
MarisKX <maris.kurpnieks@gmail.com>
2023-09-12 16:25:33 +0200

    Created devcontainer for work with github workspaces

--- MyGarage/MyGarage/settings.py
+++ MyGarage/MyGarage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z)k0(+02rxs3@6*v^3--cqqp9ae1jb3uc=##z!&n3xhz0hclje
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     # Custom apps:
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
