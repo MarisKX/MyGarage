f7b31bdc1b786155406dc29b79c2dfcde631f99e
MarisKX <maris.kurpnieks@gmail.com>
2023-09-15 23:31:28 +0200

    Fixed lint error

--- MyGarage/MyGarage/urls.py
+++ MyGarage/MyGarage/urls.py
@@ -3,7 +3,8 @@ MyGarage URL Configuration
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -19,3 +20,9 @@ urlpatterns = [
     ),
     path('api/user/', include('user.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
